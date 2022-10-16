3f55ae7c85fd23db1fc9866f048d73a7ba9009fa
tatatoppen <tatatoppen@gmail.com>
2022-10-16 19:02:07 +0300

    решение

---
+++ Tarea/Program.cs
@@ -0,0 +1,19 @@
+﻿// Написать программу, которая из имеющегося массива строк
+// формирует массив из строк, длина которых меньше либо равна 3 символа. 
+string[] myArray1 = new string[5] {"2", "23", "hello", "world", "text"};
+string[] myArray2 = new string[myArray1.Length];
+int count = 0;
+for (int i = 0; i < myArray1.Length; i++)
+    {
+        Console.Write($"{myArray1[i]} ");
+    }
+Console.WriteLine();
+for (int i = 0; i < myArray1.Length; i++)
+    {
+    if(myArray1[i].Length <= 3)
+        {
+        myArray2[count] = myArray1[i];
+        count++;
+        }
+    Console.Write($"{myArray2[i]} ");
+    }
